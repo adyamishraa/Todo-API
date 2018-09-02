e201a342dea6a4980796b9c7569714f0d18ed837
Adya Mishra <adya20mishra@gmail.com>
2018-09-03 00:48:40 +0530

    associate users with todos

--- server/server.js
+++ server/server.js
@@ -16,9 +16,10 @@ var app=express();
 var port=process.env.PORT ||3000;
 app.use(bodyParser.json());
 
-app.post('/todos',(req,res)=>{
+app.post('/todos',authenticate,(req,res)=>{
     var todo=new Todo({
-        text:req.body.text
+        text:req.body.text,
+        _creator:req.user._id
     });
     todo.save().then((doc)=>{
         res.send(doc);
@@ -27,21 +28,26 @@ app.post('/todos',(req,res)=>{
     })
 });
 
-app.get('/todos',(req,res)=>{
-    Todo.find().then((todos)=>{
+app.get('/todos',authenticate,(req,res)=>{
+    Todo.find({_creator:req.user._id
+    }).then((todos)=>{
+        
 res.send({todos});
     },(e)=>{
 res.status(400).send(e);
     })
 })
 
-app.get('/todos/:id',(req,res)=>{
+app.get('/todos/:id',authenticate,(req,res)=>{
     var id=req.params.id;
     if(!ObjectId.isValid(id)){
         return res.status(404).send();
     }
 
-Todo.findById(id).then((todo)=>{
+Todo.findById({
+    _id:id,
+    _creator:req.user._id
+}).then((todo)=>{
 if(!todo){
 res.status(404).send();
 }
@@ -56,7 +62,10 @@ app.delete('/todos/:id',(req,res)=>{
     }
 })
 
-Todo.findByIdAndRemove(id).then((todo)=>{
+Todo.findOneAndRemove({
+_id:id,
+_creator:req.user._id
+}).then((todo)=>{
 if(!todo){
 return res.status(404).send();
 }
@@ -66,7 +75,7 @@ res.send(todo);
 })
 })
 
-app.patch('/todos/:id',(req,res)=>{
+app.patch('/todos/:id',authenticate,(req,res)=>{
     var id=req.params.id;
     var body=_.pick(req.body,['text','completed']);
 
@@ -81,7 +90,10 @@ app.patch('/todos/:id',(req,res)=>{
         body.completed=false;
         body.completedAt=null;
     }
-    Todo.findByIdAndUpdate(id,{$set: body},{new: true}).then((todo)=>{
+    Todo.findOneAndUpdate({
+        _id:id,
+        _creator:req.user._id
+    },{$set: body},{new: true}).then((todo)=>{
         if(!todo){
             return res.status(404).send();
         }
@@ -124,6 +136,14 @@ app.post('/users/login',(req,res)=>{
     }).catch((e)=>{
         res.status(400).send();
 
+    })
+})
+
+app.delete('/users/me/token', authenticate,(req,res)=>{
+    req.user.removeToken(req.token).then(()=>{
+        res.status(200).send();
+    },()=>{
+        res.status(400).send();
     })
 })
 
